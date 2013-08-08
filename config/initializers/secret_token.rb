65f58eb0153204d1ba86293455bec339680027d7
Anton Sozontov <a.sozontov@gmail.com>
2013-08-08 12:48:33 +0400

    default rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoivEmulator::Application.config.secret_token = '3ccd26fdaf659de34c76fb6e288f7b25d5c1b2f2351185f3a7c53ed5b126d9d053ebac67f4edc28279357b5ea40da08a9469a441dfcab56925938868efc93665'
