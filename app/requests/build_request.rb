a39da5e650ce30634493a808a957b4e47d2f6b06
Svetlana Vtyurina <vturina@yandex.ru>
2013-08-09 20:10:54 +0400

    заполняем xml данными с формы

--- app/requests/build_request.rb
+++ app/requests/build_request.rb
@@ -1,6 +1,9 @@
 class BuildRequest < Mustache
 
-  def initialize(name)
+  def initialize(name, params = {})
+    params.each do |key, value|
+      context[key.to_sym] = value
+    end
     self.template = read_template(name)
   end
 
@@ -8,5 +11,28 @@ class BuildRequest < Mustache
     template = statement_type + '.xml'
     File.read File.expand_path File.join('../../views/ezags', template), __FILE__
   end
+
+  %w(register_period child_birth_period father_birth_period mother_birth_period).each do |name|
+    [name + '_from', name + '_to'].each do |method|
+      define_method(method) do
+        Date.new(context[(method + '_year').to_sym].to_i,
+                 context[(method + '_month').to_sym].to_i,
+                 context[(method + '_day').to_sym].to_i)
+      end
+    end
+
+  end
+
+  def child_gender_string
+    case context[:child_gender]
+    when 'false'
+      'female'
+    when 'true'
+      'male'
+    else
+      'undefined'
+    end
+  end
+
 
 end
